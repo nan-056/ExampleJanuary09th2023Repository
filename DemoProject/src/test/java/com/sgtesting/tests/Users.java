85273fe5789e6d39ab6e8aafb3e580ad489c09dd
nan-056 <srswathi460@gmail.com>
2023-04-07 08:37:22 +0530

    Intial Draft

---
+++ DemoProject/src/test/java/com/sgtesting/tests/Users.java
@@ -0,0 +1,22 @@
+package com.sgtesting.tests;
+
+import org.testng.annotations.Test;
+
+public class Users {
+ 
+	@Test
+  public void method1()
+  {
+		System.out.println("It is Method1 Statement");
+  }
+	@Test
+	  public void method2()
+	  {
+			System.out.println("It is Method2 Statement");
+	  }
+	@Test
+	  public void method3()
+	  {
+			System.out.println("It is Method3 Statement");
+	  }
+}
